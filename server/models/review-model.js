73a121ee3d9220b02afefa66e9cc9bcb241eda45
sam chris <vsgitsam25@gmail.com>
2025-06-18 18:26:26 +0300

    made the review model

---
+++ server/models/review-model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+  },
+  { timestamps: true }
+);
+
+const Reviews =
+  mongoose.model.Reviews || mongoose.model("Reviews", reviewSchema);
+
+export default Reviews;
