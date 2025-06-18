1230041d81f5b7aa4cb83ee3ad87d4355a2463d4
sam chris <vsgitsam25@gmail.com>
2025-06-18 17:41:55 +0300

    implemented check auth for the logout

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import signup from "../controllers/auth-controllers/signup.js";
 import login from "../controllers/auth-controllers/login.js";
 import logout from "../controllers/auth-controllers/logout.js";
+import checkAuth from "../middlewares/check-auth.js";
 
 const authRoutes = Router();
 
 authRoutes.post("/signup", signup);
 authRoutes.post("/login", login);
-authRoutes.post("/logout", logout);
+authRoutes.post("/logout", checkAuth, logout);
 
 export default authRoutes;
